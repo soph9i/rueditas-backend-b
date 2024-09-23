3f352b0d9cc9bf45ad5ff2d0cfb0471cb353f352
soph9i <berruvsophia@gmail.com>
2024-09-22 21:51:33 -0500

    Backend Rueditas

---
+++ src/main/java/pe/edu/cibertec/rueditas/rueditas_backend_b/RueditasBackendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas.rueditas_backend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasBackendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasBackendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas/rueditas_backend_b/RueditasBackendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas.rueditas_backend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasBackendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
